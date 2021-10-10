018083e939876a733fb4288469261527bc89d550
Masaya Kataoka <ms.kataoka@gmail.com>
2021-10-10 14:53:30 +0900

    remove unused functions
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- python_github/workflow.py
+++ python_github/workflow.py
@@ -3,13 +3,6 @@ from python_github.github_object import GithubObject
 class Workflow(GithubObject):
     def __init__(self, token) -> None:
         super().__init__(token)
-    def get(self, repository) -> None:
+    def get(self, repository):
         url = self.base_url + "/repos/" + repository + "/actions/workflows"
         return super().request(url)
-    def get_id(self, repository, name) -> None:
-        url = self.base_url + "/repos/" + repository + "/actions/workflows"
-        response = super().request(url)
-        for workflow in response["workflows"]:
-            if workflow["name"] == name:
-                return workflow['id']
-        return None
